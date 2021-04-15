672b68ccfc44eeaece59e601418678684cea659b
Aura A. Sánchez H <aura.sanchez@timba.co>
2021-04-15 15:25:45 -0500

    Slider Volume

---
+++ Assets/Scripts/SetVolume.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetVol (float sliderValue)
+    {
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20); //Representa los valores en logaritmo
+    }
+}
