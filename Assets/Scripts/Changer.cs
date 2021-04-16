d2e0f7f23abd1b30d11d265cc7b264fa35a5bf1c
Aura A. Sánchez H <aura.sanchez@timba.co>
2021-04-16 00:10:58 -0500

    Customize character

---
+++ Assets/Scripts/Changer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Changer : MonoBehaviour
+{
+    [Header ("Sprite To Change")] //Declarar una lista
+    public SpriteRenderer bodyPart;
+
+    [Header("Sprite To Cycle Through")]
+    public List<Sprite> options = new List<Sprite>(); //Lista de sprites
+
+    private int currentOption = 0; //Opcion actual
+   
+    public void NextOption()
+    {
+        currentOption++; //Aumentar la opcion actual en 1
+        if (currentOption >= options.Count) //Verificar si hemos excedido el tamaño de la lista 
+        {
+            currentOption = 0; //y si es asi reestablecer
+        }
+
+        bodyPart.sprite = options[currentOption];
+    }
+
+    public void PreviousOption()
+    {
+        currentOption--;
+        if (currentOption <= 0)
+        {
+            currentOption = options.Count - 1;
+        }
+
+        bodyPart.sprite = options[currentOption];
+    }
+}
