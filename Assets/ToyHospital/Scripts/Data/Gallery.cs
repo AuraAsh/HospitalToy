5aa31fc3273c438fbcfcfede05e8b9dbaac1f961
Aura A. Sánchez H <aura.sanchez@timba.co>
2021-05-10 12:09:49 -0500

    Ajuste en galeria
    Queda guardado por partes

--- Assets/ToyHospital/Scripts/Data/Gallery.cs
+++ Assets/ToyHospital/Scripts/Data/Gallery.cs
@@ -30,24 +30,24 @@ public class Gallery : MonoBehaviour
             spritePlayer.Add(item.GetComponent<ChangerPlayer>().bodyPart.sprite);
         }
 
-        //    parenRefGallerySpritePlayer = Instantiate(parentSpriteInstance);
-        //    parenRefGallerySpritePlayer.transform.position = positionContentGalleryInstance.position;
-        //    parenRefGallerySpritePlayer.transform.SetParent(positionContentGalleryInstance);
-        //    parenRefGallerySpritePlayer.transform.localScale = Vector3.one;
+            parenRefGallerySpritePlayer = Instantiate(parentSpriteInstance);
+            parenRefGallerySpritePlayer.transform.position = positionContentGalleryInstance.position;
+            parenRefGallerySpritePlayer.transform.SetParent(positionContentGalleryInstance);
+            parenRefGallerySpritePlayer.transform.localScale = Vector3.one;
 
-        //    for (int i = 0; i < spritePlayer.Count; i++)
-        //    {
-        //        refGallerySpritePlayer = Instantiate(spriteInstance);
-        //        refGallerySpritePlayer.transform.position = parenRefGallerySpritePlayer.transform.position;
-        //        refGallerySpritePlayer.transform.SetParent(parenRefGallerySpritePlayer.transform);
-        //        refGallerySpritePlayer.transform.localScale = Vector3.one;
+            for (int i = 0; i < spritePlayer.Count; i++)
+            {
+                refGallerySpritePlayer = Instantiate(spriteInstance);
+                refGallerySpritePlayer.transform.position = parenRefGallerySpritePlayer.transform.position;
+                refGallerySpritePlayer.transform.SetParent(parenRefGallerySpritePlayer.transform);
+                refGallerySpritePlayer.transform.localScale = Vector3.one;
 
-        //        imageGallery.Add(refGallerySpritePlayer); 
-        //    }
+                imageGallery.Add(refGallerySpritePlayer); 
+            }
 
-        //    for (int i = 0; i < imageGallery.Count; i++)
-        //    {
-        //        imageGallery[i].sprite = saveManager.changer[i].GetComponent<ChangerPlayer>().bodyPart.sprite;
-        //    }
+            for (int i = 0; i < imageGallery.Count; i++)
+            {
+                imageGallery[i].sprite = saveManager.changer[i].GetComponent<ChangerPlayer>().bodyPart.sprite;
+            }
         }
     }
